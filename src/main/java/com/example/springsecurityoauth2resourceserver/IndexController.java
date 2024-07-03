fa23b79a112232954140f591340c91f3ebd8256e
onlydev7777 <onlydev7777@gmail.com>
2024-07-03 16:02:23 +0900

    first commit

---
+++ src/main/java/com/example/springsecurityoauth2resourceserver/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.springsecurityoauth2resourceserver;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public Authentication index(Authentication authentication){
+        return authentication;
+    }
+}
