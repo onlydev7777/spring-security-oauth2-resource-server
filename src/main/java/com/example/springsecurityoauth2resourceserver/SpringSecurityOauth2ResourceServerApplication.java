91422f448ca29298ca0c21c4cd728619db7a5e0c
onlydev7777 <onlydev7777@gmail.com>
2024-07-03 16:01:39 +0900

    first commit

---
+++ src/main/java/com/example/springsecurityoauth2resourceserver/SpringSecurityOauth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityoauth2resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ResourceServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityOauth2ResourceServerApplication.class, args);
+  }
+
+}
